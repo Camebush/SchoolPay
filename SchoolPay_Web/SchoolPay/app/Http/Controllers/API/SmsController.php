1a4334f88776bb3df9254f2b87ca4003f6c349fb
CameBush <85628326+Camebush@users.noreply.github.com>
2022-09-28 15:37:08 +0100

    Update SmsController.php
    Correction de la fonction de confirmation du sms

--- SchoolPay_Web/SchoolPay/app/Http/Controllers/API/SmsController.php
+++ SchoolPay_Web/SchoolPay/app/Http/Controllers/API/SmsController.php
@@ -24,24 +24,36 @@ class SmsController extends Controller
     )
     {
 
-        $datetime = now()->toDateTimeString();
+        $username = 'makaebenezer@yahoo.fr';
+        $transaction_url = 'https://smsvas.com/bulk/public/index.php/api/v1/sendsms';
+        $password = 'ecoleenspd2022';
+        $senderid = 'SchoolPay';
+        $mobiles = self::getPhoneNumbers($studentPhoneNumber, $payerPhoneNumber);
+        $sms = "Paiement des droits universitaires ({$universityRight}) de l'étudiant {$studentName} ({$registerNumber}) effectué par {$payerPhoneNumber} le {$datetime}.\nMontant: {$amount} , Frais: 100 FCFA.\n\nInstitut universitaire: {$schoolName}.";
 
         $client = new Client();
 
-        $sms = "Paiement des droits universitaires ($universityRight) de l'étudiant $studentName ($registerNumber) effectué par $payerPhoneNumber le $datetime.\nMontant: $amount , Frais: 100 FCFA.\n\nInstitut universitaire:$schoolName.";
 
-        $response = $client->request('POST',
-            'https://smsvas.com/bulk/public/index.php/api/v1/sendsms',
-            [
-                'form_params' => [
-                    'username' => 'makaebenezer@yahoo.fr',
-                    'password' => 'ecoleenspd2022',
-                    'senderid' => 'SchoolPay',
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        try {
+           $client->request('POST', $transaction_url, [
+                'headers' => $headers,
+                'json' => [
+                    'username' => $username,
+                    'password' => $password,
+                    'senderid' => $senderid,
                     'sms' => $sms,
-                    'mobiles' => self::getPhoneNumbers($studentPhoneNumber, $payerPhoneNumber)
-                ]
-            ]
-        );
+                    'mobiles' => $mobiles,
+                ],
+            ]);
+
+        } catch (RequestException $ex) {
+            throw new CollectionRequestException('Request to pay transaction - unsuccessful.', 0, $ex);
+        }
 
     }
 
